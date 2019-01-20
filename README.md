f118181165993e677cb0276edf9ee85e3008cc7f
mds5000 <mds5000@gmail.com>
2019-01-19 19:42:06 -0800

    Converted to InfluxDB

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # aquarium-server
 An aquarium controller and monitor application
+
+
+# Design Documentation
+
+ * Install Scripts
+    - Install packages (apt & python)
+    - Create system services
+    - Compile front-end
+
+ * InfluxDB Server & Telegraph Services
+ * Webserver - aio-http
+ * Drivers - 
+
+## Drivers
+PWM -> rpi use kernel PWM functionality, create a test PWM class that serves as an indicator
+GPIO -> use kernel PMW
+TEMP -> use kernel 1-wire 
+
+
+A: Temperature logger
+ - full query API
+ - hardcoded sensor logged to DB
+ - graph widget UI
+B: GPIO controller
+ - hardcoded GPIO devices
+ - change events logged to DB
+C: PWM controller
+ - hardcoded devices
+ - change events logged to DB
+D: PWM profile
+ - profile API
+ - time sync
+ - profile with interpolation
+
+x mock devices for testing
+x temp device driver/task
+x push to influxdb
+x front-end API
+x test database 
+- configuration database
+- config database test
+- temp sensor test cases
+
+- deploy temp sensor to pi
+- implement GPIO driver
+- front-end API
+- add logging
+
+
+# Database
+- devices (pwm, gpio, temp, ... analog_in, )
+- Modules (group of devices matching template)
+   - kessil
+   - dosing
+   - alarm
+
+# API
+/device {get, post, /id/}
+   /device/id/ {get, post, delete}
+   /device/id/value {get, post}
+/module {get, post, /id/}
+   /module/id/ {get, post, delete}
+   /module/id/dashboard {get}
+   /module/id/ ... specific module API
--- backend/app.py
+++ backend/app.py
@@ -1,211 +1,43 @@
-"""
-To run:
-    $ export FLASK_APP=server.py
-    $ export FLASK_ENV=development
-    $ flask run (or python -m flask run)
-"""
-from flask import Flask, jsonify, send_from_directory, request
-from flask_mqtt import Mqtt
-from schema import db, Device, Measurement, Event
+import asyncio
+import os
 
-import json
+from aiohttp import web 
+from aioinflux import InfluxDBClient
 
-app = Flask(__name__)
-
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-db.init_app(app)
-
-app.config['MQTT_CLIENT_ID'] = 'server'
-app.config['MQTT_BROKER_URL'] = 'msunderland-lin'
-app.config['MQTT_BROKER_PORT'] = 1883
-mqtt = Mqtt()
-mqtt.init_app(app)
-
-@app.route('/static/<path:path>')
-def static_server(path):
-    return send_from_directory('../app', path)
-
-@app.route('/api/devices')
-def devices_api():
-    return jsonify([d.serialize() for d in Device.query.all()])
-
-@app.route('/api/device', methods=['GET'])
-def device_api():
-    """ URL Parameters:
-    id=0
-    name='name'
-    address='provider/device'
-    """
-    filter = {}
-    if 'id' in request.args:
-        filter = {"id": request.args['id']}
-    elif 'address' in request.args:
-        filter = {"address": request.args['address']}
-    elif 'name' in request.args:
-        filter = {"name": request.args['name']}
-    else:
-        return "Must provide one of 'id', 'name', or 'address' as query parameter.", 400
-
-    try:
-        device = Device.query.filter_by(**filter).one()
-        return jsonify(device.serialize())
-    except NoResultFound:
-        return "No device found matching query.", 404
-    except MultipleResultsFound:
-        return "Multiple devices found matching query.", 404
-
-@app.route('/api/device/<int:id>', methods=['POST', 'DELETE'])
-def device_modify_api(id):
-    if request.method == "DELETE":
-        Device.query.filter_by(id=id).delete()
-        db.session.commit()
-        return str(id), 200
-    
-    else:
-        updated_device = request.get_json()
-        updated_device.pop('address', None)  # Can't modify device's address
-        updated_device.pop('id', None)       # or it's ID
-        Device.query.filter_by(id=id).update(updated_device)
-        db.session.commit()
-        return jsonify(Device.query.get(id).serialize())
-
-@app.route('/api/measurements')
-def measurements_api():
-    """ URL Parameters:
-    from: (DateTime) Return data collected after this date.
-    to: (DateTime) Return data collected up to this date.
-    id: (int[,...]) Return data for devices with 'id', multiple allowed
-    include_comments: Return measurement comments with the data.
-
-    Response:
-        [
-            {"id": 0, "timestamp": t0, "value": value, "comment": comment},
-            {"id": 1, "timestamp": t1, "value": value, "comment": null},
-            ...
-        ]
-    """
-    columns = [Measurement.id, Measurement.timestamp, Measurement.value]
-    if 'include_comments' in request.args:
-        columns.append(Measurement.comment)
-
-    query = db.session.query(*columns).order_by(Measurement.timestamp)
-
-    if 'id' in request.args:
-        query = query.filter(Measurement.id.in_(request.args["id"]))
-
-    if 'from' in request.args:
-        query = query.filter(Measurement.timestamp > request.args["from"])
-
-    if 'to' in request.args:
-        query = query.filter(Measurement.timestamp < request.args["from"])
-
-    return jsonify(query.all())
-
-@app.route("/api/measurement", methods=["POST"])
-def measurement_api():
-    payload = request.get_json()
-
-    measurement = Measurement(
-        device=payload["id"],
-        value=payload["value"],
-        comment=payload.get("comment")
-    )
-    db.session.add(measurement)
-    db.session.commit()
-
-    return jsonify(measurement.serialize())
+from temp_sensor import TempSensor
 
 
-@app.route('/api/events')
-def events_api():
-    """ URL Parameters:
-    from: (DateTime) Return data collected after this date.
-    to: (DateTime) Return data collected up to this date.
-    id: (int[,...]) Return data for devices with 'id', multiple allowed
-    Response:
-        [
-            {"id": 0, "timestamp": t0, "value": value, "comment": comment},
-            {"id": 1, "timestamp": t1, "value": value, "comment": null},
-            ...
-        ]
-    """
-    columns = [Event.id, Event.timestamp, Event.value, Event.comment]
-    query = db.session.query(*columns).order_by(Event.timestamp)
+async def initialize_database(app):
+    db_host = os.getenv("INFLUXDB_HOST", "localhost")
+    db_port = os.getenv("INFLUXDB_PORT", 8086)
+    db_database = os.getenv("ENVIRONMENT", "aquarium")
+    db = InfluxDBClient(host=db_host, port=db_port, db=db_database)
+    app["influx-db"] = db
+    await db.create_database(db=db_database)
 
-    if 'id' in request.args:
-        query = query.filter(Event.id.in_(request.args["id"]))
+async def initialize_application(app):
+    loop = asyncio.get_event_loop()
+    for driver in app['drivers']:
+        loop.create_task(driver.event_handler(app))
+        app.add_routes(driver.routes())
 
-    if 'from' in request.args:
-        query = query.filter(Event.timestamp > request.args["from"])
-
-    if 'to' in request.args:
-        query = query.filter(Event.timestamp < request.args["from"])
-
-    return jsonify(query.all())
+async def shutdown_application(app):
+    drivers = app.get('drivers', [])
+    done, pending = await asyncio.wait(drivers, timeout=5.0)
+    for task in pending:
+        print("Failed to stop Task", task)
 
 
-@mqtt.on_connect()
-def handle_mqtt_connection(client, userdata, flags, ret_code):
-    app.logger.info("Connected to MQTT Broker: %s", client)
+if __name__ == '__main__':
+    from tests.fake_temp import MockTempSensor
+    mock = MockTempSensor()
+    app = web.Application()
+    app['drivers'] = [
+        TempSensor(path=mock.root+"/hwmon0/temp1_input")
+    ]
 
-    with app.app_context():
-        app.logger.info("Loading existing devices from database:")
-        for device in Device.query.all():
-            add_new_device(device.address)
+    app.on_startup.append(initialize_application)
+    app.on_startup.append(initialize_database)
+    #app.on_shutdown.append(shutdown_application)
 
-    mqtt.subscribe("new_device")
-
-@mqtt.on_message()
-def handle_mqtt_message(client, userdata, message):
-    topic = message.topic
-    payload = json.loads(message.payload)
-    app.logger.debug("MQTT Message: %s, %s", topic, message.payload)
-
-    with app.app_context():
-
-        if topic == "new_device":
-            device = Device.query.filtery_by(address=payload["name"]).one_or_none()
-            if device is None:
-                device = Device(
-                    address=payload["name"],
-                    description=payload.get("description", ""),
-                    units=payload.get("units")
-                )
-                db.session.add(device)
-                db.session.commit()
-            add_new_device(device.address)
-
-        elif topic.endswith("/value"):
-            address = topic.rsplit("/", 1)[0]
-            device = Device.query.filter_by(address=address).one()
-            measurement = Measurement(
-                device=device.id,
-                value=payload["value"],
-                comment=payload.get("comment")
-            )
-            db.session.add(measurement)
-            db.session.commit()
-
-        elif topic.endswith("/update"):
-            address = topic.rsplit("/", 1)[0]
-            device = Device.query.filter_by(address=address).one()
-            event = Event(
-                device=device.id,
-                action=payload["action"],
-                value=payload["value"],
-                comment=payload.get("comment")
-            )
-            db.session.add(event)
-            db.session.commit()
-
-        else:
-            app.logger.warn("Unhandled MQTT Message '%s'", topic)
-
-
-def add_new_device(address):
-    app.logger.info("Register Device '%s'", address)
-    mqtt.subscribe("{}/value".format(address))
-    mqtt.subscribe("{}/update".format(address))
-
-
+    web.run_app(app)
